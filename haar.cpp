a31b4c17ca47210c97f04f6e481437244394039e
William Simião <williamsimiao@gmail.com>
2017-06-30 14:36:48 -0300

    implementacao da grade e iniciando calculo e do conjunto de colunas que nao contem faces

--- haar.cpp
+++ haar.cpp
@@ -9,6 +9,11 @@
 using namespace std;
 using namespace cv;
 
+struct coluna {
+  bool temRosto;
+  int centroX;
+};
+
 /** Function Headers */
 void detectAndDisplay( Mat frame );
 
@@ -17,6 +22,114 @@ String face_cascade_name, eyes_cascade_name;
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 String window_name = "Capture - Face detection";
+//by:Will
+Point daEsquerda;
+Point daDireita;
+int colsNumber = 5;
+float colWidth;
+std::vector<coluna> colunaVect;
+
+
+Point pontoMedio( Point esquerdo, Point direito) {
+  int x = (esquerdo.x + direito.x)/2;
+  int y = (esquerdo.y + direito.y)/2;
+  Point meio(x, y);
+  return meio;
+}
+
+Point calculaCentro(Rect oneFace) {
+  Point center( oneFace.x + oneFace.width/2, oneFace.y + oneFace.height/2 );
+  return center;
+}
+
+void escolhePontosExtremos(std::vector<Rect> faces) {
+  int maior;
+  int menor;
+  if(faces.size() != 0) {
+    maior = faces[0].x + faces[0].width/2;
+    menor = faces[0].x + faces[0].width/2;
+
+    int maiorInd = 0;
+    int menorInd = 0;
+    for(int i = 0; i < faces.size(); i++) {
+      Point center = calculaCentro(faces[i]);
+      if(center.x < menor) {
+        menor = center.x;
+        menorInd = i;
+      }
+      if(center.x > maior) {
+        maior = center.x;
+        maiorInd = i;
+      }
+    }
+
+    if(faces.size() != 0) {
+      daEsquerda = calculaCentro(faces[menorInd]);
+      daDireita = calculaCentro(faces[maiorInd]);
+    }
+  }
+}
+
+void calculaRetangulosDispoiniveisEDesenha(Mat frame) {
+  int nConsecutivas = 0;
+  int indiceDaprimeira = 0;
+  int nConsecutivasFinal = 0;
+  int indiceDaprimeiraFinal = 0;
+  bool emSequencia = false;
+
+  for(int j = 0; j < colsNumber; j++) {
+
+    if(!colunaVect[j].temRosto) {
+      if(!emSequencia) {
+        indiceDaprimeira = j;
+      }
+      emSequencia = true;
+      nConsecutivas++;
+    }
+    else{
+      emSequencia = false;
+      nConsecutivas = 0;
+    }
+    if(nConsecutivas > nConsecutivasFinal) {
+      nConsecutivasFinal = nConsecutivas;
+      indiceDaprimeiraFinal = indiceDaprimeira;
+    }
+  }
+
+  //Agora vamos desenhar um retangulo da extremidade esquerda da primeira
+  //coluna ate a extremidade direita da ultima
+  printf("nConsecutivasFinal: %d\n", nConsecutivasFinal);
+  printf("ponto um: x=%d , y=%d\n",colunaVect[indiceDaprimeiraFinal].centroX, 0);
+  printf("ponto dois: x=%d , y=%d\n",colunaVect[nConsecutivasFinal-1].centroX, frame.cols );
+  rectangle(frame, Point(colunaVect[indiceDaprimeiraFinal].centroX, 0),
+            Point(colunaVect[nConsecutivasFinal-1].centroX, frame.cols),
+            Scalar( 0, 0, 255 ),
+            1,
+            0);
+
+}
+
+void inicializaColunas(Mat frame) {
+  //Largura do frame dividido pelo numero de colunas
+  colunaVect.resize(colsNumber);
+  colWidth = frame.cols/colsNumber;
+  //O inicio sera no ponto  colWidth/2
+  //Apos essa variavel mantem referencia para o centro calculado da ultima coluna
+  int centroDeColuna = colWidth/2;
+  for(int i = 0; i < colsNumber; i++) {
+    colunaVect[i].centroX = centroDeColuna;
+    centroDeColuna += colWidth;
+    colunaVect[i].temRosto = false;
+  }
+}
+
+bool isFaceInsideColuna(coluna myColuna, Rect face) {
+  Point centro = calculaCentro(face);
+  int diferenca = abs(centro.x - myColuna.centroX);
+  if(diferenca < colWidth/2) return true;
+  return false;
+}
+
 
 /** @function main */
 int main( int argc, const char** argv )
@@ -42,9 +155,14 @@ int main( int argc, const char** argv )
     if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
 
     //-- 2. Read the video stream
-    capture.open( 0 );
+    // capture.open( 0 );
+    capture = VideoCapture("people_walking.mp4");
+
     if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
 
+    inicializaColunas(frame);
+    // auto primeiro = high_resolution_clock::now();
+    int count;
     while ( capture.read(frame) )
     {
         if( frame.empty() )
@@ -55,6 +173,9 @@ int main( int argc, const char** argv )
 
         //-- 3. Apply the classifier to the frame
         detectAndDisplay( frame );
+        calculaRetangulosDispoiniveisEDesenha(frame);
+        imshow( window_name, frame );
+        count++;
 
         char c = (char)waitKey(10);
         if( c == 27 ) { break; } // escape
@@ -74,24 +195,40 @@ void detectAndDisplay( Mat frame )
     //-- Detect faces
     face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
 
+    // //by:Will
+    // printf("faces.size(): %lu\n", faces.size());
+    // escolhePontosExtremos(faces);
+    // printf("doido\n" );
+    // Point pMedio = pontoMedio(daEsquerda, daDireita);
+    // printf("x: %d , y: %d\n", pMedio.x, pMedio.y);
+    // int radius = 40;
+    // circle( frame, pMedio, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+
+
     for ( size_t i = 0; i < faces.size(); i++ )
     {
         Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
         ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
 
-        Mat faceROI = frame_gray( faces[i] );
-        std::vector<Rect> eyes;
-
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-        for ( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        for(int j = 0; j < colsNumber; j++) {
+          if(isFaceInsideColuna(colunaVect[j], faces[i])) {
+            colunaVect[j].temRosto = true;
+            //Se presumirmos que cada cabeca so pode ocupar uma coluna pode parar
+            //break;
+          }
         }
+        // Mat faceROI = frame_gray( faces[i] );
+        // std::vector<Rect> eyes;
+        //
+        // //-- In each face, detect eyes
+        // eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+        //
+        // for ( size_t j = 0; j < eyes.size(); j++ )
+        // {
+        //     Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+        //     int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+        //     circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        // }
     }
     //-- Show what you got
-    imshow( window_name, frame );
 }
